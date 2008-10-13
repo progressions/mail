2992c00a159897555b8239393904c3665f1a4cfd
Jeff Coleman <progressions@gmail.com>
2008-10-13 12:31:23 -0500

    Added nifty scaffold and Fetcher plugin)

--- app/controllers/backup_controller.rb
+++ app/controllers/backup_controller.rb
@@ -1,2 +1,9 @@
+require 'net/pop'
+
 class BackupController < ApplicationController
+  def index
+    Net::POP3.start('pop.gmail.com', 995,
+                'worldracketeeringsquad@gmail.com', 'sylvia69') do |email|
+    end
+  end
 end
